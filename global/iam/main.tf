35bacdea9e878166d02fb12782cf8582f979162e
Kenny Cheang <kenny.cheang@opsguru.io>
2022-03-03 18:04:14 -0800

    updated AMI

--- global/iam/main.tf
+++ global/iam/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "claim_dev_server" {
-  ami           = "ami-02354e95b39ca8dec"
+  ami           = "ami-00656d51d312f0dee"
   instance_type = "t2.micro"
   tags = {
     Name = "claim_dev_server"
